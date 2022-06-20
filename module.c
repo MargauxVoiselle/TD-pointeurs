38189435476b49e7abc2a5954a6bbfd78ba2148d
Margaux Voiselle <margaux@MacBook-Air-de-Margaux.local>
2022-06-20 14:54:27 +0200

    changements après commentaire

--- module.c
+++ module.c
@@ -2,37 +2,38 @@
 
 // donne la longueur d'une chaîne
 int length(char* string){
-    int stringLength = 1;
-    char stringCharacter = *string;
-    while (stringCharacter != '\0'){
-        stringCharacter = *(string + stringLength);
+    int stringLength = 0;
+    while (*string){
+        string = string + 1;
         stringLength++;
     }
-    stringLength--; // on retire le '\0'
     return stringLength;
 }
 
 // copie une chaîne dans un espace mémoire disponible
 void copyString(char* stringToCopy, char* stringCopied){
-    char stringCharacter = *stringToCopy;
+    //char stringCharacter = *stringToCopy;
     int count = 0;
-    while (stringCharacter != '\0'){
-        *(stringCopied + count) = stringCharacter;
+    while (*stringToCopy){
+        *(stringCopied + count) = *stringToCopy;
         count++;
-        stringCharacter = *(stringToCopy + count);
+        stringToCopy = stringToCopy + 1;
     }
 }
 
 // compte le nombre de 'e' dans une chaîne
 int numberOfE(char *string){
-    char stringCharacter = *string;
     int countOfE = 0, i = 0;
-    while (stringCharacter != '\0'){
-        if (stringCharacter == 'e'){
+    while (*string){
+        if (*string == 'e'){
             countOfE++;
         }
-        i++;
-        stringCharacter = *(string + i);
+        string = string + 1;
     }
     return countOfE;
+}
+
+// modifie un caractère d'une chaîne selon la position donnée
+void modifyCharacter(char *stringToModify, char newCharacter, int position){
+    *(stringToModify + position) = newCharacter;
 }
--- module.h
+++ module.h
@@ -1,3 +1,4 @@
 int length(char* string);
 void copyString(char* stringToCopy, char* stringCopied);
 int numberOfE(char *string);
+void modifyCharacter(char *stringToModify, char newCharacter, int position);
